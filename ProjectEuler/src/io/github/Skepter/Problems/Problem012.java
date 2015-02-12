7178d2dd98612149af462aa983fca2cfacd8e75e
Skepter <skepter.misc@gmail.com>
2015-02-12 17:42:40 +0000

    FINALLY Completed problem 12

--- ProjectEuler/src/io/github/Skepter/Problems/Problem012.java
+++ ProjectEuler/src/io/github/Skepter/Problems/Problem012.java
@@ -1,38 +1,45 @@
 package io.github.Skepter.Problems;
 
 import io.github.Skepter.Utils.Incomplete;
+import io.github.Skepter.Utils.RT;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class Problem012 implements Incomplete {
+public class Problem012 extends RT implements Incomplete {
 
 	//What is the value of the first triangle number to have over five hundred divisors?
 	public static void main(final String[] args) {
-		final long old = System.currentTimeMillis();
 		//calculate triangle numbers
 		//1, 3, 6, 10, 15, 21, 28, 36, 45, 55, ...
 		// 1 + 2 + 3 + 4 + 5 + 6 etc.
 
-		int n = Integer.MAX_VALUE;
 		int count = 0;
-		for (int i = 1; i <= n; i++) {
+//		int max = 0;
+
+		int numberOfFactors = 0;
+		for (int i = 1;; i++) {
 			count += i;
-			if (getFactors(count).size() >= 500) {
-				n = i;
+
+			if (numberOfFactors < getFactors(count)) {
+				numberOfFactors = getFactors(count);
+			}
+
+			if (getFactors(count) >= 500) {
+//				max = i;
 				break;
 			}
 		}
-		System.out.println(getFactors(count).size() + " factors");
-		System.out.println(n + "th term: " + count);
-		System.out.println((System.currentTimeMillis() - old) + "ms");
+//		System.out.println(getFactors(count) + " factors");
+//		System.out.println(max + "th term: " + count);
+		System.out.println(count);
+		uptime();
 	}
 
-	public static List<Integer> getFactors(final int j) {
-		final List<Integer> list = new ArrayList<Integer>();
-		for (int i = 1; i <= j; i++)
-			if ((j % i) == 0)
-				list.add(i);
-		return list;
+	public static int getFactors(int j) {
+		int count = 0;
+		for (int i = 1; i <= Math.sqrt(j); i++) {
+			if ((j % i) == 0) {
+				count++;
+			}
+		}
+		return count *= 2;
 	}
 }
