4f6fe0d008e235d04c5ad04a85d26c349db0885b
Jorel Ali (Skepter) <skepter.misc@gmail.com>
2017-05-03 18:44:15 +0100

    Completed problem 50

--- ProjectEuler/src/io/github/Skepter/Problems/Problem050.java
+++ ProjectEuler/src/io/github/Skepter/Problems/Problem050.java
@@ -1,36 +1,68 @@
 package io.github.Skepter.Problems;
 
+import java.util.List;
+
+import io.github.Skepter.ForeignUtils.SieveWithBitset;
+import io.github.Skepter.Utils.LoadingBar;
 import io.github.Skepter.Utils.RT;
-import io.github.Skepter.Utils.Utils;
 
 public class Problem050 extends RT {
 
-	/**/
+	/* https://projecteuler.net/problem=50
+	 * 93337ms
+	 * */
 	public static void main(final String[] args) {
+		List<Integer> primes = SieveWithBitset.sieveOfEratosthenes(1000000);
+//		int count = 0;
+//		for(int i = 0; i < 21; i++) {
+//			count += primes.get(i);
+//		}
+//		System.out.println(count);
+//		
+		
 		//maximise sumOfPrimes(a) - sumOfPrimes(b);
-		int count = 0;
-		for(int i = 0; i < 1000; i++) {
-			for(int j = 0; j < 1000; j++) {
-				if(count < (sumOfPrimes(i) - sumOfPrimes(j))) {
-					if(count > 1000000) {
-						System.out.println(count);
-						uptime();
-						return;
-					}
-					count = sumOfPrimes(i) - sumOfPrimes(j);
+		
+		
+		//int count = 0;
+		
+		int max = 0;
+		int maxi = 0;
+		int maxj = 0;
+		
+		LoadingBar b = new LoadingBar(10000);
+		
+		for(int i = 0; i < 10000; i++) {
+			//Assuming that j is less than 100
+			for(int j = 0; j < 100; j++) {
+				int consecutiveSum = sumOfPrimes(i) - sumOfPrimes(j);
+				if(consecutiveSum <= 0) {
+					continue;
 				}
+				
+				if(primes.contains(consecutiveSum)) {
+					if(consecutiveSum > max) {
+						max = consecutiveSum;
+						maxi = i;
+						maxj = j;
+					}
+					//System.out.println(consecutiveSum);
+				}
+				
 			}
+			b.updateBar(i);
+			
 		}
-		System.out.println(count);
+		System.out.println(max + ": Using primes between " + maxj + " and " + maxi);
+		
+//		System.out.println(count);
 		uptime();
 	}
 	
 	public static int sumOfPrimes(int max) {
+		List<Integer> primes = SieveWithBitset.sieveOfEratosthenes(max);
 		int count = 0;
-		for(int i = 1; i < max; i++){
-			if(Utils.isPrime(i)) {
-				count += i;
-			}
+		for(int i : primes){
+			count += i;
 		}
 		return count;
 	}
