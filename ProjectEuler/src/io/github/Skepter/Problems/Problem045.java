ae8eaa02af3ca198518c8c15fb3aac7c52aadbd0
Jorel Ali (Skepter) <skepter.misc@gmail.com>
2017-05-04 07:12:09 +0100

    Improved problem45 time (again)

--- ProjectEuler/src/io/github/Skepter/Problems/Problem045.java
+++ ProjectEuler/src/io/github/Skepter/Problems/Problem045.java
@@ -14,41 +14,36 @@ public class Problem045 extends RT {
 	 * 
 	 * Find the next triangle number that is also pentagonal and hexagonal.
 	 * 
-	 * Program took 01 minutes, 26 seconds, 892 milliseconds :/
+	 * Program took 43 seconds, 008 milliseconds :/
 	 */
 	public static void main(final String[] args) {
 		long MAX = 100000;
 		List<Long> triangles = new ArrayList<Long>();
 		List<Long> pentagons = new ArrayList<Long>();
 		List<Long> hexagons = new ArrayList<Long>();
-		
-		for(long i = 1; i <= MAX; i++) {
+
+		for (long i = 1; i <= MAX; i++) {
 			triangles.add(getTriangle(i));
 			pentagons.add(getPentagonal(i));
 			hexagons.add(getHexagonal(i));
 		}
-	
-		//int count = 0;
-		List<Long> triangles2 = new ArrayList<Long>();
-		triangles2.addAll(triangles);
-		System.out.println("Removing pentagons");
-		triangles.removeAll(pentagons);
-		System.out.println("Updating triangles");
-		triangles2.removeAll(triangles);
-		System.out.println("Removing hexagons");
-		triangles.removeAll(hexagons);
-		Utils.printList(triangles2);
-//		for(long i : triangles) {
-//			
-//			if(pentagons.contains(i) && hexagons.contains(i)) {
-//				count++;
-//				System.out.println(i);	
-//				if(count == 3) {
-//					break;
-//				}
-//			}
-//		}
+
+		// int count = 0;
 		
+		triangles.retainAll(pentagons);
+		triangles.retainAll(hexagons);
+		Utils.printList(triangles);
+		// for(long i : triangles) {
+		//
+		// if(pentagons.contains(i) && hexagons.contains(i)) {
+		// count++;
+		// System.out.println(i);
+		// if(count == 3) {
+		// break;
+		// }
+		// }
+		// }
+
 		uptime();
 	}
 
@@ -59,7 +54,7 @@ public class Problem045 extends RT {
 	private static long getPentagonal(long i) {
 		return (i * (3 * i - 1)) / 2;
 	}
-	
+
 	private static long getHexagonal(long i) {
 		return i * (2 * i - 1);
 	}
