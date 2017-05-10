71555aea9a5b3dd7946b734e54bf563dad1b7c64
Jorel Ali (Skepter) <skepter.misc@gmail.com>
2017-05-10 23:09:22 +0100

    Solved problem 99 using logarithms :D

--- ProjectEuler/src/io/github/skepter/problems/Problem099.java
+++ ProjectEuler/src/io/github/skepter/problems/Problem099.java
@@ -1,6 +1,5 @@
 package io.github.skepter.problems;
 
-import java.math.BigInteger;
 import java.util.List;
 
 import io.github.skepter.utils.RT;
@@ -11,15 +10,15 @@ public class Problem099 extends RT {
 	/* https://projecteuler.net/problem=99 */
 	public static void main(final String[] args) {
 		List<String> list = Utils.readFromFile("p099_base_exp.txt");
-		BigInteger max = BigInteger.ZERO;
+		double max = 0.0D;
 		int line = 0;
-		for(int i = 0; i <= 999; i++) {
-			BigInteger a = BigInteger.valueOf(Long.valueOf(list.get(i).split(",")[0]));
-			int b = Integer.parseInt(list.get(i).split(",")[1]);
-			BigInteger exponent = a.pow(b);
-			if(exponent.compareTo(max) > 0) {
-				max = exponent;
-				line = i;
+		for (int i = 0; i <= 999; i++) {
+			double a = Double.parseDouble(list.get(i).split(",")[0]);
+			double b = Double.parseDouble(list.get(i).split(",")[1]);
+			double out = b * Math.log(a);
+			if (out > max) {
+				max = out;
+				line = i + 1; // Stupid OBOEs
 			}
 		}
 		System.out.println(line);
