4c536e7a84fc4fd298f2ac7b14c2a1b5eabd7f30
Jorel Ali (Skepter) <skepter.misc@gmail.com>
2017-05-13 12:52:44 +0100

    Solved problem 107

--- ProjectEuler/src/io/github/skepter/problems/Problem107.java
+++ ProjectEuler/src/io/github/skepter/problems/Problem107.java
@@ -1,113 +1,176 @@
 package io.github.skepter.problems;
 
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 import io.github.skepter.utils.RT;
 import io.github.skepter.utils.Utils;
 
 public class Problem107 extends RT {
 
-	//Second attempt
+	//Third attempt
 	
-	/* https://projecteuler.net/problem=107 
-	 * Prim's Algorithm. */
-	public static void main(final String[] args) {
-		
-		//Turns the matrix from the file into an int[][]
-		int[][] matrix = new int[40][40];
-		List<String> list = Utils.readFromFile("p107_network.txt");
-		for(int i = 0; i < 40; i++) {
-			for(int j = 0; j < 40; j++) {
-				if(list.get(i).split(",")[j].equals("-")) {
-					matrix[i][j] = 1000;
-				} else {
-					matrix[i][j] = Integer.parseInt(list.get(i).split(",")[j]);
+		/* https://projecteuler.net/problem=107 
+		 * Prim's Algorithm. 
+		 * 
+		 * Program took 092 milliseconds */
+		public static void main(final String[] args) {
+			
+			//Turns the matrix from the file into an int[][]
+			int[][] matrix = new int[40][40];
+			List<String> list = Utils.readFromFile("p107_network.txt");
+			for(int i = 0; i < 40; i++) {
+				for(int j = 0; j < 40; j++) {
+					if(list.get(i).split(",")[j].equals("-")) {
+						matrix[i][j] = 1000;
+					} else {
+						matrix[i][j] = Integer.parseInt(list.get(i).split(",")[j]);
+					}
 				}
 			}
-		}
-		
-		//Gets the total weight of the network
-		int totalWeight = 0;
-		for(int row = 0; row < 40; row++) {
-			for(int col = 0; col < 40; col++) {
-				if(matrix[col][row] != 1000)
-					totalWeight += matrix[col][row]; 
-			}
-		}
-		totalWeight = totalWeight / 2;
-		
-		//start at row 0. Go until you find the min value.
-		//Exclude row 0 and the column with the min value.
-		//Go to the ROW which is the COLUMN of the min value
-
-		Set<Integer> ignoredColumns = new HashSet<Integer>();
-		
-		//There may be a row which has a repeated number :/
-		
-		int count = 0;
-		int row = 0;
-		int minWeight = 0;
-		while (count < 40) {
-			int minValue = 1000;
-			int colWithMinValue = -1;
 			
-			//Loop through each column from the current row to find the minimum value
-			for(int col = 0; col < 40; col++) {
-				if(matrix[row][col] < minValue) {
-					minValue = matrix[row][col];
-					colWithMinValue = col;
+			//Gets the total weight of the network
+			int totalWeight = 0;
+			for(int row = 0; row < 40; row++) {
+				for(int col = 0; col < 40; col++) {
+					if(matrix[col][row] != 1000)
+						totalWeight += matrix[col][row]; 
 				}
-				matrix[row][col] = 1000;
 			}
+			totalWeight = totalWeight / 2;
 			
-			//ignoredColumns.add(colWithMinValue);
-			//ignoredColumns.add(row);
+			/*
+			 * So I realised that you have to actually check EACH ROW in a cumulative manner
+			 * 
+			 */
 			
-//			for(int i = 0; i < 40; i++) {
-//				System.out.println(Arrays.toString(matrix[i]));
+			List<Integer> rowsToCheck = new ArrayList<Integer>();
+			int count = 0;
+			rowsToCheck.add(0); //Start checking the first row
+			while(rowsToCheck.size() != 40) {
+				int minValue = 1000;
+				int colWithMinValue = -1;
+				
+				for(int row : rowsToCheck) {
+					
+					for(int col = 0; col < 40; col++) {
+						if(rowsToCheck.contains(col)) {
+							continue;
+						}
+						
+						if(matrix[row][col] < minValue) {
+							minValue = matrix[row][col];
+							colWithMinValue = col;
+						}
+					}
+										
+				}
+				if(colWithMinValue == -1) {
+					//This case should never occur
+					System.out.println("Error");
+				}
+				count += minValue;
+				rowsToCheck.add(colWithMinValue);
+				
+			}
+			System.out.println(totalWeight - count);
+			uptime();
+		}
+	
+	
+	
+	
+	
+	
+	
+//	//Second attempt
+//	
+//	/* https://projecteuler.net/problem=107 
+//	 * Prim's Algorithm. */
+//	public static void main(final String[] args) {
+//		
+//		//Turns the matrix from the file into an int[][]
+//		int[][] matrix = new int[40][40];
+//		List<String> list = Utils.readFromFile("p107_network.txt");
+//		for(int i = 0; i < 40; i++) {
+//			for(int j = 0; j < 40; j++) {
+//				if(list.get(i).split(",")[j].equals("-")) {
+//					matrix[i][j] = 1000;
+//				} else {
+//					matrix[i][j] = Integer.parseInt(list.get(i).split(",")[j]);
+//				}
 //			}
-
-			if(colWithMinValue == -1) {
-				colWithMinValue = 22;
-//				Utils.printListSingleLine(ignoredColumns);
-//				count++;
-//				System.out.println(minWeight);
-//				System.out.println(totalWeight - minWeight);
-//				return;
-			}
-			
-			for(int i = 0; i < 40; i++) {
-				System.out.println("count = " + count);
-				matrix[i][colWithMinValue] = 1000;
-			}
-			for(int i = 0; i < 40; i++) {
-				matrix[i][row] = 1000;
-			}
-			count++;
-			row = colWithMinValue;
-			minWeight += minValue;
-		}
-		System.out.println(minWeight);
-		System.out.println(totalWeight - minWeight);
-
-		
-		uptime();
-	}
+//		}
+//		
+//		//Gets the total weight of the network
+//		int totalWeight = 0;
+//		for(int row = 0; row < 40; row++) {
+//			for(int col = 0; col < 40; col++) {
+//				if(matrix[col][row] != 1000)
+//					totalWeight += matrix[col][row]; 
+//			}
+//		}
+//		totalWeight = totalWeight / 2;
+//		
+//		//start at row 0. Go until you find the min value.
+//		//Exclude row 0 and the column with the min value.
+//		//Go to the ROW which is the COLUMN of the min value
+//
+//		Set<Integer> ignoredColumns = new HashSet<Integer>();
+//		
+//		//There may be a row which has a repeated number :/
+//		
+//		int count = 0;
+//		int row = 0;
+//		int minWeight = 0;
+//		while (count < 40) {
+//			int minValue = 1000;
+//			int colWithMinValue = -1;
+//			
+//			//Loop through each column from the current row to find the minimum value
+//			for(int col = 0; col < 40; col++) {
+//				if(matrix[row][col] < minValue) {
+//					minValue = matrix[row][col];
+//					colWithMinValue = col;
+//				}
+//				matrix[row][col] = 1000;
+//			}
+//			
+//			//ignoredColumns.add(colWithMinValue);
+//			//ignoredColumns.add(row);
+//			
+////			for(int i = 0; i < 40; i++) {
+////				System.out.println(Arrays.toString(matrix[i]));
+////			}
+//
+//			if(colWithMinValue == -1) {
+//				colWithMinValue = 22;
+////				Utils.printListSingleLine(ignoredColumns);
+////				count++;
+////				System.out.println(minWeight);
+////				System.out.println(totalWeight - minWeight);
+////				return;
+//			}
+//			
+//			for(int i = 0; i < 40; i++) {
+//				System.out.println("count = " + count);
+//				matrix[i][colWithMinValue] = 1000;
+//			}
+//			for(int i = 0; i < 40; i++) {
+//				matrix[i][row] = 1000;
+//			}
+//			count++;
+//			row = colWithMinValue;
+//			minWeight += minValue;
+//		}
+//		System.out.println(minWeight);
+//		System.out.println(totalWeight - minWeight);
+//
+//		
+//		uptime();
+//	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+//////////////////////////////////////////////////////////////////////////////////////////////////
 	
 	//First attempt
 	
