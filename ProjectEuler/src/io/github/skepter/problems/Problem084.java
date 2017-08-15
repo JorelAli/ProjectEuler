b7f28a03daf992e3e0e2af6dd038dfb9f565e49f
Jorel Ali (Skepter) <skepter.misc@gmail.com>
2017-08-15 01:15:39 +0100

    Attempt to simulate monopoly

---
+++ ProjectEuler/src/io/github/skepter/problems/Problem084.java
@@ -0,0 +1,149 @@
+package io.github.skepter.problems;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.concurrent.ThreadLocalRandom;
+
+import io.github.skepter.utils.RT;
+
+public class Problem084 extends RT {
+
+	final static int DICE_SIZE = 6;
+	final static int TRIALS = 1000000;
+	
+	static int doublesMade = 0;
+	static int currentSquare = 0;
+		
+	
+	static HashMap<Integer, Integer> squareLandCount = new HashMap<Integer, Integer>();
+	
+	/* https://projecteuler.net/problem=84 
+	 * What are the top 3 most likely squares to land on in a game of monopoly
+	 * if you used two 4 sided dice instead of two 6 sided dice? */
+	public static void main(final String[] args) {
+		
+		for(int i = 0; i < TRIALS; i++) {
+			rollDice();
+			if(currentSquare == 2 || currentSquare == 17 || currentSquare == 33) {
+				doCommunityChest(); //do community chest
+			} else if(currentSquare == 30) {
+				currentSquare = 10; //go to jail
+			} else if(currentSquare == 7 || currentSquare == 22 || currentSquare == 36) {
+				doChance();
+			}
+			
+			//System.out.println("new current square is " + currentSquare);
+			endTurn();
+		}
+		
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		for(Entry<Integer, Integer> entry : squareLandCount.entrySet()) {
+			System.out.println(entry.getKey() + ": " + entry.getValue());
+			list.add(entry.getValue());
+		}
+		Collections.sort(list);
+		for(int i : list.subList(list.size() - 3, list.size()))
+			System.out.println(i);
+		
+		uptime();
+	}
+	
+	public static void rollDice() {
+		boolean doubleMade = false;
+		int dice1 = ThreadLocalRandom.current().nextInt(1, DICE_SIZE + 1);
+		int dice2 = ThreadLocalRandom.current().nextInt(1, DICE_SIZE + 1);
+		
+		//System.out.println("Rolled a " + dice1 + " and a " + dice2);
+		//System.out.println("Current square is " + currentSquare);
+		
+		if(dice1 == dice2) {
+			doubleMade = true;
+			doublesMade++;
+			
+			if(doublesMade == 3) {
+				doublesMade = 0;
+				currentSquare = 10;
+				return;
+			}
+		}
+		
+		currentSquare = currentSquare + dice1 + dice2;
+		if (currentSquare > 39) {
+			currentSquare = currentSquare % 40;
+		}
+		
+		if(doubleMade) {
+			rollDice();
+		}
+		
+	}
+	
+	public static void doChance() {
+		int val = ThreadLocalRandom.current().nextInt(1, 17);
+		switch(val) {
+			case 1:
+				currentSquare = 0;
+				break;
+			case 2:
+				currentSquare = 10;
+				break;
+			case 3:
+				currentSquare = 11;
+				break;
+			case 4:
+				currentSquare = 24;
+				break;
+			case 5:
+				currentSquare = 39;
+				break;
+			case 6:
+				currentSquare = 5;
+				break;
+			case 7:
+			case 8:
+				if(currentSquare == 7) {
+					currentSquare = 15;
+				} else if(currentSquare == 22) {
+					currentSquare = 25;
+				} else if(currentSquare == 36) {
+					currentSquare = 5;
+				}
+				break;
+			case 9:
+				if(currentSquare == 7) {
+					currentSquare = 12;
+				} else if(currentSquare == 22) {
+					currentSquare = 28;
+				} else if(currentSquare == 36) {
+					currentSquare = 12;
+				}
+				break;
+			case 10:
+				currentSquare -= 3;
+				break;
+			default:
+				return;
+		}
+	}
+	
+	public static void doCommunityChest() {
+		int val = ThreadLocalRandom.current().nextInt(1, 17);
+		if(val == 1) {
+			currentSquare = 0;
+		} else if(val == 2) {
+			currentSquare = 10;
+		} else {
+			return;
+		}
+	}
+	
+	public static void endTurn() {
+		squareLandCount.put(currentSquare, squareLandCount.getOrDefault(currentSquare, 0) + 1);
+		//add current square to square land count
+	}
+	
+	
+}
--- ProjectEuler/src/io/github/skepter/problems/Problem145.java
+++ ProjectEuler/src/io/github/skepter/problems/Problem145.java
@@ -1,11 +1,12 @@
 package io.github.skepter.problems;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+
 import io.github.skepter.utils.Incomplete;
 import io.github.skepter.utils.RT;
 import io.github.skepter.utils.Utils;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Problem145 extends RT implements Incomplete {
 
@@ -22,69 +23,138 @@ public class Problem145 extends RT implements Incomplete {
 
 	Program took 2411669ms
 	*/
+	
+	static Pattern pattern = Pattern.compile("0|2|4|6|8");
 
-	static int i = 0;
-
-	public static void main(final String[] args) throws InterruptedException {
-		List<Thread> threads = new ArrayList<Thread>();
-		Thread thread = new Thread() {
-			public void run() {
-				i += findReversables(0, 1000000);
+	public static void main(final String[] args) {
+		
+		/*
+		 * Odd number + odd number = even
+		 * Even number + even number = even
+		 * 
+		 * Odd + even = odd.
+		 */
+		
+		
+		
+		
+		
+		
+		
+		int count = 0;
+		Set<Integer> integerSet = new HashSet<Integer>();
+		//only have to check the numbers from 1 to 500 million
+		for(int i = 1; i <= 500000000; i++) {
+			if(i == 1000000) {
+				uptime();
 			}
-		};
-		Thread thread1 = new Thread() {
-			public void run() {
-				i += findReversables(1000001, 10000000);
+				
+			if(!integerSet.contains(i)) {
+				int reverse = Utils.getReverse(i);
+				int num = i + reverse;
+				
+				boolean even = isEven(i);
+				boolean reverseEven = isEven(reverse);
+				
+				if(even ^ reverseEven) {
+					//if(!isEven(num)) {
+						if(isAllOdd(num)) {
+							integerSet.add(i);
+							integerSet.add(reverse);
+							count++;
+						}
+					//}
+				}
 			}
-		};
-		Thread thread2 = new Thread() {
-			public void run() {
-				i += findReversables(10000001, 100000000);
-			}
-		};
-		Thread thread3 = new Thread() {
-			public void run() {
-				i += findReversables(100000001, 1000000000);
-			}
-		};
-		threads.add(thread);
-		threads.add(thread1);
-		threads.add(thread2);
-		threads.add(thread3);
-		for (Thread t : threads) {
-			t.start();
-			t.join();
 		}
-		System.out.println(i);
+		System.out.println(count);
 		uptime();
 	}
-
-	public static int findReversables(int min, int max) {
-		int count = -5;
-		for (int i = min; i <= max; i++) {
-			if (isEven(i) && isEven(Utils.getReverse(i)))
-				continue;
-			if (isAllOdd(i + Utils.getReverse(i))) {
-				count++;
-			}
-		}
-		return count;
+	
+	private static boolean isEven(int i) {
+		return i % 2 == 0;
 	}
-
-	public static boolean isEven(int number) {
-		if (number % 2 == 0)
-			return true;
-		return false;
+	
+	public static boolean isAllOdd(int n) {
+		return !(pattern.matcher(String.valueOf(n)).matches());
 	}
-
-	public static boolean isAllOdd(int number) {
-		boolean odd = true;
-		String s = String.valueOf(number);
-		for (int i = 0; i < s.length(); i++) {
-			if (isEven(Integer.parseInt(String.valueOf(s.charAt(i))))) {
-				odd = false;
-			}
-		}
-		return odd;
-	}
+	
+//	public static boolean isAllOdd(int number) {
+//	boolean odd = true;
+//	String s = String.valueOf(number);
+//	if(s.mat)
+//	for (int i = 0; i < s.length(); i++) {
+//		if (isEven(Integer.parseInt(String.valueOf(s.charAt(i))))) {
+//			odd = false;
+//		}
+//	}
+//	return odd;
+//}
+	
+//  FIRST ATTEMPT
+// 
+//	static int i = 0;
+//
+//	public static void main(final String[] args) throws InterruptedException {
+//		List<Thread> threads = new ArrayList<Thread>();
+//		Thread thread = new Thread() {
+//			public void run() {
+//				i += findReversables(0, 1000000);
+//			}
+//		};
+//		Thread thread1 = new Thread() {
+//			public void run() {
+//				i += findReversables(1000001, 10000000);
+//			}
+//		};
+//		Thread thread2 = new Thread() {
+//			public void run() {
+//				i += findReversables(10000001, 100000000);
+//			}
+//		};
+//		Thread thread3 = new Thread() {
+//			public void run() {
+//				i += findReversables(100000001, 1000000000);
+//			}
+//		};
+//		threads.add(thread);
+//		threads.add(thread1);
+//		threads.add(thread2);
+//		threads.add(thread3);
+//		for (Thread t : threads) {
+//			t.start();
+//			t.join();
+//		}
+//		System.out.println(i);
+//		uptime();
+//	}
+//
+//	public static int findReversables(int min, int max) {
+//		int count = -5;
+//		for (int i = min; i <= max; i++) {
+//			if (isEven(i) && isEven(Utils.getReverse(i)))
+//				continue;
+//			if (isAllOdd(i + Utils.getReverse(i))) {
+//				count++;
+//			}
+//		}
+//		return count;
+//	}
+//
+//	public static boolean isEven(int number) {
+//		if (number % 2 == 0)
+//			return true;
+//		return false;
+//	}
+//
+//	public static boolean isAllOdd(int number) {
+//		boolean odd = true;
+//		String s = String.valueOf(number);
+//		for (int i = 0; i < s.length(); i++) {
+//			if (isEven(Integer.parseInt(String.valueOf(s.charAt(i))))) {
+//				odd = false;
+//			}
+//		}
+//		return odd;
+//	}
 }
