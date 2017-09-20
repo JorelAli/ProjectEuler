0219479641d654f0d5408ddccd87eb19920f0f10
Jorel Ali (Skepter) <skepter.misc@gmail.com>
2017-09-20 20:45:11 +0100

    Stated strategy required for problem 345 (Hungarian Algorithm)

---
+++ ProjectEuler/src/io/github/skepter/problems/Problem345.java
@@ -0,0 +1,14 @@
+package io.github.skepter.problems;
+
+import io.github.skepter.utils.RT;
+
+public class Problem345 extends RT {
+
+	/* https://projecteuler.net/problem=345
+	 * Matrix Sum
+	 * 
+	 * Strategy: Hungarian Algorithm for maximising */
+	public static void main(final String[] args) {
+		uptime();
+	}
+}
--- ProjectEuler/src/io/github/skepter/problems/Problem493.java
+++ ProjectEuler/src/io/github/skepter/problems/Problem493.java
@@ -1,12 +1,11 @@
 package io.github.skepter.problems;
 
-import io.github.skepter.utils.Abandoned;
-import io.github.skepter.utils.RT;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
+
+import io.github.skepter.utils.Abandoned;
+import io.github.skepter.utils.RT;
 
 public class Problem493 extends RT implements Abandoned {
 
@@ -20,28 +19,23 @@ public class Problem493 extends RT implements Abandoned {
 
 	//Answer uses combinatorics. Not Advanced Enough
 	public static void main(final String[] args) {		
-		int runs = 1000; // 10 million
+		int runs = 10000000; // 10 million
 		double c = 0;
 
+		/* Fill urn with balls */
+		List<Integer> urn = new ArrayList<Integer>();
+		for (int i = 0; i < 7; i++) {
+			for (int j = 0; j < 10; j++) {
+				urn.add(i);
+			}
+		}
+		
 		for (int m = 0; m < runs; m++) {
-			List<Integer> urn = new ArrayList<Integer>();
 
-			/* Fill urn with balls */
-			Random random = new Random();
-			for (int i = 0; i < 7; i++) {
-				for (int j = 0; j < 10; j++) {
-					urn.add(i);
-				}
-			}
-
+			/* Mix them all up and choose 20 random balls*/
+			Collections.shuffle(urn);
 			List<Integer> balls = urn.subList(0, 20);
-
-			for (int i = 0; i < 20; i++) {
-				int selection = random.nextInt(70);
-				balls.add(urn.get(selection));
-				// urn.remove(selection);
-			}
-
+			
 			// int count = 0;
 			for (int i = 0; i < 7; i++) {
 				if (Collections.frequency(balls, i) != 0) {
@@ -80,7 +74,7 @@ public class Problem493 extends RT implements Abandoned {
 		// }
 		System.out.println(c);
 		System.out.println(c / runs);
-		System.out.println(c / runs / 20);
+		System.out.println(c / runs / 7);
 		uptime();
 	}
 
