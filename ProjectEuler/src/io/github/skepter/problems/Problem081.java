6c602f33ba3492ea352d089b0fda1f34580d6aee
Jorel Ali <skepter.misc@gmail.com>
2017-09-30 20:51:56 +0100

    Problem 81 initial commit. Adds eclipse project files

---
+++ ProjectEuler/src/io/github/skepter/problems/Problem081.java
@@ -0,0 +1,18 @@
+package io.github.skepter.problems;
+
+import io.github.skepter.utils.RT;
+
+public class Problem081 extends RT {
+
+	/* https://projecteuler.net/problem=81 */
+	public static void main(final String[] args) {
+		
+		//TODO: Do the test with the 5x5 grid,
+		// Then using the same code, apply it for the 80x80 grid
+		
+		//Strategies: Use the dynamic programming from problem 18 and 67
+		
+		
+		uptime();
+	}
+}
