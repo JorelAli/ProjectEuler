3afde2efd6ed4a7a930c6d111d1da6dbd7d4b041
Jorel Ali (Skepter) <skepter.misc@gmail.com>
2018-01-17 02:00:36 +0000

    Changed algorithm to use Pick's Theorem
    It's a bit faster, but still > 1m (I think) in runtime. At least I won't have to wait hours now...

--- ProjectEuler/src/io/github/skepter/problems/Problem504.java
+++ ProjectEuler/src/io/github/skepter/problems/Problem504.java
@@ -26,12 +26,13 @@ public class Problem504 extends RT {
 		/*
 		 * Suppose m = n.
 		 * The number of quarilaterals = n^4
+		 * = 100_000_000 quadrilaterals
 		 */
 		
 		Utils.assert_(new Quad(1, 1, 1, 1).contains(0, 0));
 		
 		
-		int m = 4;
+		int m = 100;
 		LoadingBar bar = new LoadingBar("Problem 504", m);
 		int square = 0;
 		for(int a = 1; a <= m; a++) {
@@ -40,17 +41,18 @@ public class Problem504 extends RT {
 				for(int c = 1; c <= m; c++) {
 					for(int d = 1; d <= m; d++) {
 						Quad quad = new Quad(a, b, c, d);
+						
 						//List of points from (-4, -4) to (4, 4)
 						//(-m, -m) to (m, m)
-						int count = 0;
-						for(int x = -m; x <= m; x++) {
-							for(int y = -m; y <= m; y++) {
-								if(quad.contains(x, y)) {
-									count++;
-								}
-							}
-						}
-						if(Utils.isInteger(Math.sqrt(count))) {
+//						int count = 0;
+//						for(int x = -m; x <= m; x++) {
+//							for(int y = -m; y <= m; y++) {
+//								if(quad.contains(x, y)) {
+//									count++;
+//								}
+//							}
+//						}
+						if(Utils.isInteger(Math.sqrt(quad.latticePoints()))) {
 							square++;
 						}
 					}
@@ -80,7 +82,48 @@ public class Problem504 extends RT {
 		 * https://en.wikipedia.org/wiki/Pick%27s_theorem
 		 * Using Pick's theorem, rearrange to find i?
 		 */
-		
+		public float latticePoints() {
+			//Where A = area
+			//b = lattice points on the boundary
+			//return A + 1 - (b / 2);
+			
+			/*
+			 * Area can be calculated using the "4 triangles" in each quadrant
+			 */
+			float area = 0;
+			area += ((a + b) / 2);
+			area += ((a + d) / 2);
+			area += ((c + d) / 2);
+			area += ((b + c) / 2);
+			
+			int boundaries = 0;
+			//Red line
+			for(float i = -c; i <= 0; i++) {
+				if(Utils.isInteger((b / c) * i + b)) {
+					boundaries++;
+				}
+			}
+			//Black line
+			for(float i = -c; i <= 0; i++) {
+				if(Utils.isInteger((d / -c) * i - d)) {
+					boundaries++;
+				}
+			}
+			//Purple line
+			for(float i = 0; i <= a; i++) {
+				if(Utils.isInteger((-b / a) * i + b)) {
+					boundaries++;
+				}
+			}
+			//Green line
+			for(float i = 0; i <= a; i++) {
+				if(Utils.isInteger((d / a) * i - d)) {
+					boundaries++;
+				}
+			}
+			
+			return area + 1 - (boundaries / 2);
+		}
 		
 		
 		public boolean contains(float x, float y) {
