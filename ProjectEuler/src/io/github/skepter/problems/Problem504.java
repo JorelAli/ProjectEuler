9ffe3589f38404bdd171091fa248e9ccf096eceb
Jorel Ali (Skepter) <skepter.misc@gmail.com>
2018-01-17 01:32:51 +0000

    Implemented 504

--- ProjectEuler/src/io/github/skepter/problems/Problem504.java
+++ ProjectEuler/src/io/github/skepter/problems/Problem504.java
@@ -1,6 +1,7 @@
 package io.github.skepter.problems;
 
 import io.github.skepter.utils.RT;
+import io.github.skepter.utils.Utils;
 
 public class Problem504 extends RT {
 
@@ -23,25 +24,73 @@ public class Problem504 extends RT {
 		 * Suppose m = n.
 		 * The number of quarilaterals = n^4
 		 */
+		
+		Utils.assert_(new Quad(1, 1, 1, 1).contains(0, 0));
+		
+		int m = 4;
+		int square = 0;
+		for(int a = 1; a <= m; a++) {
+			for(int b = 1; b <= m; b++) {
+				for(int c = 1; c <= m; c++) {
+					for(int d = 1; d <= m; d++) {
+						Quad quad = new Quad(a, b, c, d);
+						//List of points from (-4, -4) to (4, 4)
+						//(-m, -m) to (m, m)
+						int count = 0;
+						for(int x = -m; x <= m; x++) {
+							for(int y = -m; y <= m; y++) {
+								if(quad.contains(x, y)) {
+									count++;
+								}
+							}
+						}
+						if(Utils.isInteger(Math.sqrt(count))) {
+							square++;
+						}
+					}
+				}
+			}
+		}
+		System.out.println(square);
+		
 		uptime();
 	}
 	
-	class Quad {
+	
+	//https://www.desmos.com/calculator/4gldcisifb
+	static class Quad {
 		
-		private int a, b, c, d;
+		private double a, b, c, d;
 		
-		public Quad(int a, int b, int c, int d) {
+		public Quad(double a, double b, double c, double d) {
 			this.a = a;
 			this.b = b;
 			this.c = c;
 			this.d = d;
 		}
 		
-		public boolean contains(int x, int y) {
-			/*
-			 * Using gradients and inequalities... oh geez
-			 */
-			return false;
+		public boolean contains(double x, double y) {
+			return satisfiesAB(x, y) && satisfiesBC(x, y) && satisfiesCD(x, y) && satisfiesDA(x, y);
+		}
+		
+		//Purple line
+		private boolean satisfiesAB(double x, double y) {
+			return y < (-b / a) * x + b;
+		}
+		
+		//Red line		
+		private boolean satisfiesBC(double x, double y) {
+			return y < (b / c) * x + b;
+		}
+		
+		//Black line
+		private boolean satisfiesCD(double x, double y) {
+			return y > (d / -c) * x - d;
+		}
+		
+		//Green line
+		private boolean satisfiesDA(double x, double y) {
+			return y > (d / a) * x - d;
 		}
 	}
 }
