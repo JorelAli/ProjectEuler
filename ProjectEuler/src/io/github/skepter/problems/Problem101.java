92553576c5433db5a19a454d8ef2814f43982d1e
Jorel Ali <jorel.k.ali@gmail.com>
2018-02-27 22:40:16 +0000

    Begun creating sequence analyser
    This analyser plans to convert sequences to polynomial strings (until then, I'll figure out what to do - probably just converting it to an int[] representing coefficients)

--- ProjectEuler/src/io/github/skepter/problems/Problem101.java
+++ ProjectEuler/src/io/github/skepter/problems/Problem101.java
@@ -1,6 +1,11 @@
 package io.github.skepter.problems;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import io.github.skepter.utils.RT;
+import io.github.skepter.utils.Utils;
 
 public class Problem101 extends RT {
 
@@ -24,7 +29,7 @@ public class Problem101 extends RT {
 //			System.out.println(tenDegPoly(i));
 		
 		
-		
+		System.out.println(analyseSequence(Arrays.asList(new Integer[] {1, 8, 27})));
 		
 		
 		uptime();
@@ -36,5 +41,54 @@ public class Problem101 extends RT {
 	
 	public static int pow(int i, int pow) {
 		return (int) Math.pow(i, pow);
+	}
+	
+	public static String analyseSequence(List<Integer> sequence) {
+		return analyseSequence(sequence, "");
+	}
+	
+	public static String analyseSequence(List<Integer> sequence, String currentSequence) {
+		String output = "";
+		
+		
+		//Determine the polynomial degree by the length of the sequence
+		int polynomialDegree = sequence.size() - 1;
+		
+		if(polynomialDegree == 1) {
+			int n = (sequence.get(1) - sequence.get(0));
+			output = n + "n";
+			
+			int lastTerm = sequence.get(0) - n;
+			//Basically the base case.
+			output = output + lastTerm;
+		} else {
+			//recursion recursion recursion!
+			
+			//Find the final "recursive difference" (at the bottom of differences)
+					
+			List<Integer> tempSequence = new ArrayList<>(sequence);
+			List<Integer> differences = new ArrayList<>();
+			
+			while(true) {
+				for (int i = 0; i < tempSequence.size() - 1; i++) {
+					differences.add(tempSequence.get(i + 1) - tempSequence.get(i));
+				}
+				
+				if(differences.size() != 1) {
+					tempSequence = new ArrayList<>(differences);
+					differences = new ArrayList<>();
+				} else {
+					break;
+				}
+			}
+			int firstTerm = differences.get(0) / Utils.factorial(polynomialDegree);
+			System.out.println(firstTerm + "n^" + polynomialDegree);
+			
+		}
+		
+		
+		
+		
+		return currentSequence + output;
 	}
 }
