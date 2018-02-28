a9d26d4fdc7e0f7808f710376b8ed0f2b81c7f80
Jorel Ali <jorel.k.ali@gmail.com>
2018-02-28 17:49:33 +0000

    Update 104

--- ProjectEuler/src/io/github/skepter/problems/Problem104.java
+++ ProjectEuler/src/io/github/skepter/problems/Problem104.java
@@ -1,6 +1,5 @@
 package io.github.skepter.problems;
 
-import java.math.BigDecimal;
 import java.math.BigInteger;
 
 import io.github.skepter.utils.RT;
@@ -15,41 +14,62 @@ public class Problem104 extends RT {
 		//Utils.isNPandigital("", 1, 9);
 		
 		
+		BigInteger n1 = BigInteger.ONE;
+		BigInteger n2 = BigInteger.ONE;
+		
+		BigInteger fn = BigInteger.ONE;
+		
+
+		for(int i = 3;;i++) {
+			fn = n1.add(n2);
+			n1 = n2;
+			n2 = fn;
+			try {
+				if (first(fn.toString()) && last(fn.toString())) {
+					System.out.println(i);
+					break;
+				}
+			} catch (Exception e) {
+			}
+			System.out.println(i);
+		}
+		
+		
 		
 		//System.out.println(fibonacci(2749));
 		
-		System.out.println(last(betterFib(541).toString()));
-		System.out.println(first(betterFib(2749).toString()));
-		
-		
-		for(int i = 3; ; i++) {
-			try {
-				if(last(betterFib(i).toString()) && first(betterFib(i).toString())) {
-					System.out.println("Result: " + i);
-					break;
-				}
-			} catch(Exception e) {}
-			System.out.println(i);
-		}
+//		System.out.println(last(betterFib(541).toString()));
+//		System.out.println(first(betterFib(2749).toString()));
+//		
+//		
+//		for(int i = 3; ; i++) {
+//			try {
+//				if(last(betterFib(i).toString()) && first(betterFib(i).toString())) {
+//					System.out.println("Result: " + i);
+//					break;
+//				}
+//			} catch(Exception e) {}
+//			System.out.println(i);
+//		}
 		
 		
 		uptime();
 	}
 	
-	static BigDecimal rt5 = Utils.nRootNewtonRaphson(BigDecimal.valueOf(5), BigDecimal.valueOf(2.236), 2, 50, 200);
-	
-	static BigDecimal phi = BigDecimal.ONE.add(new BigDecimal(Math.sqrt(5))).divide(new BigDecimal(2));
-	static BigDecimal psi = BigDecimal.ONE.subtract(new BigDecimal(Math.sqrt(5))).divide(new BigDecimal(2));
-	
-	public static BigInteger betterFib(int k) {
-		return BigDecimal.ONE.add(rt5).divide(new BigDecimal(2)).pow(k).divide(rt5, BigDecimal.ROUND_HALF_UP).add(BigDecimal.valueOf(0.5D)).toBigInteger();
-	}
-	
-	public static BigInteger fibonacci(int k) {
-		BigDecimal result = phi.pow(k).subtract(psi.pow(k)).divide(new BigDecimal(Math.sqrt(5)));
-		return result.toBigInteger();
-	}
-	
+//	static BigDecimal rt5 = Utils.nRootNewtonRaphson(BigDecimal.valueOf(5), BigDecimal.valueOf(2.236), 2, 50, 200);
+//	
+//	static BigDecimal phi = BigDecimal.ONE.add(new BigDecimal(Math.sqrt(5))).divide(new BigDecimal(2));
+//	static BigDecimal psi = BigDecimal.ONE.subtract(new BigDecimal(Math.sqrt(5))).divide(new BigDecimal(2));
+//	
+//	public static BigInteger betterFib(int k) {
+//		return BigDecimal.ONE.add(rt5).divide(new BigDecimal(2)).pow(k).divide(rt5, BigDecimal.ROUND_HALF_UP).add(BigDecimal.valueOf(0.5D)).toBigInteger();
+//	}
+//	
+//	public static BigInteger fibonacci(int k) {
+//		BigDecimal result = phi.pow(k).subtract(psi.pow(k)).divide(new BigDecimal(Math.sqrt(5)));
+//		return result.toBigInteger();
+//	}
+//	
 	public static boolean first(String str) {
 		return (Utils.isNPandigital(str.substring(0, 9), 1, 9));
 	}
