c4e47beb81186cd072f7f8d74cd1efd1899d20c2
Jorel Ali <jorel.k.ali@gmail.com>
2018-03-10 10:15:42 +0000

    Update problem 7

--- ProjectEuler/src/io/github/jorelali/problems/Problem007.java
+++ ProjectEuler/src/io/github/jorelali/problems/Problem007.java
@@ -1,29 +1,15 @@
 package io.github.jorelali.problems;
 
-import java.util.ArrayList;
-import java.util.List;
+import io.github.jorelali.foreignutils.SieveWithBitset;
+import io.github.jorelali.utils.RT;
 
-public class Problem007 {
+public class Problem007 extends RT{
 
 	//What is the 10001st prime number?
+	//Program took 094 milliseconds
 	public static void main(final String[] args) {
-		final List<Integer> list = new ArrayList<Integer>();
-		for (int i = 2; i < Integer.MAX_VALUE; i++)
-			if (isPrime(i)) {
-				if (list.size() == 10001)
-					break;
-				list.add(i);
-			}
-		System.out.println(list.get(list.size() - 1));
-	}
-	
-	//19030ms
-
-	//checks whether an int is prime or not.
-	static boolean isPrime(final int n) {
-		for (int i = 2; i < n; i++)
-			if ((n % i) == 0)
-				return false;
-		return true;
+		//Zero indexed, so 10001 - 1
+		System.out.println(SieveWithBitset.sieveOfEratosthenes(500000).get(10001 - 1));
+		uptime();
 	}
 }
