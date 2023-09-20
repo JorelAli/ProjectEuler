84b46d31976f2919179fb6a114dce3c99e687a99
Jorel Ali <github@jorel.dev>
2023-09-20 16:17:21 +0100

    Solved Problem 73

--- ProjectEuler/src/io/github/jorelali/problems/Problem073.java
+++ ProjectEuler/src/io/github/jorelali/problems/Problem073.java
@@ -1,66 +1,71 @@
 package io.github.jorelali.problems;
 
-import java.util.List;
-
-import io.github.jorelali.foreignutils.SieveWithBitset;
 import io.github.jorelali.utils.RT;
-import io.github.jorelali.utils.Utils;
 
 public class Problem073 extends RT {
 
-	/* https://projecteuler.net/problem=71 
+	/* https://projecteuler.net/problem=73
+	 * 
+	 * How many fractions lie between 1/3 and 1/2 in the sorted set
+	 * of reduced proper fractions for d <= 12,000?
 	 * 
 	 * https://en.wikipedia.org/wiki/Farey_sequence
 	 * 
-	 * */
+	 * Using the implementation of the farey sequence generator
+	 * from https://en.wikipedia.org/wiki/Farey_sequence#Next_term
+	 * converted to Java. You have to make sure you keep track of
+	 * the "old" values so you don't mangle everything up, unlike
+	 * Python that can handle it with tuples.
+	 * 
+	 * Program took 143 milliseconds
+	 */
 	
-	
-	static void farey(int n, boolean descending) {
+	// I originally wrote this for problem 72 but didn't even need it
+	// Glad I needed it this time!
+	public static void fareySequence(int n, boolean descending) {
 		int a = 0;
 		int b = 1;
 		int c = 1;
 		int d = n;
-		
-		if(descending) {
+
+		if (descending) {
 			a = 1;
-			c = n-1;
+			c = n - 1;
 		}
-		System.out.println(a + " " + b);
-		while((c <= n && !descending) || (a > 0 && descending)) {
-			int k = (n + b) / d;
-			a = c;
-			b = d;
-			c = (k*c) - a;
-			d = (k*d) - b;
-			System.out.println(a + " " + b);
+
+		int count = 0;
+		boolean startCounting = false;
+
+		int k = 0;
+		while (c <= n) {
+			k = (n + b) / d;
+
+			int oldA = a;
+			int oldB = b;
+			int oldC = c;
+			int oldD = d;
+
+			a = oldC;
+			b = oldD;
+			c = k * oldC - oldA;
+			d = k * oldD - oldB;
+			if (a == 1 && b == 2) {
+				startCounting = false;
+				break;
+			}
+			if (startCounting) {
+				count++;
+			}
+			if (a == 1 && b == 3) {
+				startCounting = true;
+			}
 		}
-	}
-	
-	static int length(int n) {
-		List<Integer> primes = SieveWithBitset.getPrimes(n);
-		return length(n, primes, 0) - 2;
-	}
-	
-	/*
-	 * Thanks to my university lecturer for teaching me about
-	 * "Tail Recursive functions" by using an accumulator to prevent running
-	 * into a StackOverflowException
-	 */
-	private static int length(int n, List<Integer> primes, int acc) {
-		if(n == 1) {
-			return acc + 2;
-		}
-		return length(n - 1, primes, acc + Utils.totient(n, primes));
+
+		System.out.println(count);
 	}
 	
 	public static void main(final String[] args) {
-		System.out.println(length(12000));
-		//farey(12000);
-		//double max = 0;
-//		for(int d = 1; d <= 12000; d++) {
-//			System.out.println(Utils.totient(d, primes));
-//			
-//		}
+		fareySequence(12000, false);
 		uptime();
 		
 	}
